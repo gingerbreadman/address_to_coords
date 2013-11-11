887a77d794b771cf8c71b42bfcf824f1debfced6
Rahim Ladak <rahim.ladak@gmail.com>
2013-11-10 21:04:52 -0600

    Completed Phase II

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 AddressToCoords::Application.routes.draw do
   get("/coords", { :controller => "addresses", :action => "fetch_coordinates" })
+  get("/coords/new_address", { :controller => "addresses", :action => "new" })
+  get("/coords/location", { :controller => "addresses", :action => "location" })
 end
